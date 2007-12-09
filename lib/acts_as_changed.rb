b12bc6a18d6bb379071e0def9ce1fbc3dc827548
joe <joe@9a1ae852-6c05-0410-aa8e-abb87183367a>
2007-12-09 18:10:29 +0000

    Backporting changes from tinymce branch related to column aliases.  Slightly improving performance for multiple aliases (which will help for bitsets). [See #26]

--- lib/acts_as_changed.rb
+++ lib/acts_as_changed.rb
@@ -183,9 +183,8 @@ module ActiveRecord
 	      # an SQL statement.
 	      def named_attributes_with_quotes(names)
 	        names.inject({}) do |quoted, name|
-	          if column = column_for_attribute(name)
-		          quoted[name] = quote_value(attributes[name.to_s], column)
-		        end
+	          column = column_for_attribute(name)
+            quoted[column.name] = quote_value(attributes[column.name.to_s], column) unless column.nil? or quoted.key?(column.name)
 	          quoted
 	        end
 	      end
